dbbd7826238da563245298d32c7b0c0d96a51ce5
unknown <reddyhanumanth2100@gmail.com>
2022-01-10 16:02:34 +0530

    initial commit

---
+++ src/main/java/com/JenkinsLearning/SpringBootAngularApplication.java
@@ -0,0 +1,14 @@
+package com.JenkinsLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularApplication.class, args);
+		System.out.println("Hello SpringBoot");
+	}
+
+}
