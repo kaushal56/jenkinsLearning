c67728f80e9c04365746614cb5ac274d740bb20f
kaushal56 <68109363+kaushal56@users.noreply.github.com>
2022-01-11 10:16:45 +0530

    Update SpringBootAngularApplication.java

--- src/main/java/com/JenkinsLearning/SpringBootAngularApplication.java
+++ src/main/java/com/JenkinsLearning/SpringBootAngularApplication.java
@@ -9,6 +9,7 @@ public class SpringBootAngularApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAngularApplication.class, args);
 		System.out.println("Hello SpringBoot");
+		System.out.println("commit2");
 	}
 
 }
