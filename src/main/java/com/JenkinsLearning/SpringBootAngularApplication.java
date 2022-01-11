b443faf924830d4e693b6640f30970cf5182da3d
unknown <reddyhanumanth2100@gmail.com>
2022-01-11 11:38:27 +0530

    war deployment chnages

--- src/main/java/com/JenkinsLearning/SpringBootAngularApplication.java
+++ src/main/java/com/JenkinsLearning/SpringBootAngularApplication.java
@@ -2,13 +2,15 @@ package com.JenkinsLearning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootAngularApplication {
+public class SpringBootAngularApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAngularApplication.class, args);
 		System.out.println("Hello SpringBoot");
 	}
+	
 
 }
